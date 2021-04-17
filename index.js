93352cfc1aeb86e178565dddf77ede28132848e9
Yusra Ahmed <isra.00287446@gmail.com>
2021-04-17 00:31:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+let append=["Milo", "Otis", "Garfield"];
+append.push("Odie");
+
+let prepend=["Milo", "Otis", "Garfield"];
+let removeLast=["Milo", "Otis", "Garfield"];
+let removeFirst=["Milo", "Otis", "Garfield"];
+
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
